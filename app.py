e381394f2b54372088416090de0fcdafb73295af
dragonfly161518 <por.nattapat.01@gmail.com>
2020-02-24 21:30:29 +0700

    post api

--- app.py
+++ app.py
@@ -5,19 +5,18 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
-@app.route('/theblindway', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    
-    return jsonify({'task': 'yepp'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        return 'Allright~'
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
