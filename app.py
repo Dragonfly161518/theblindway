68b7ec5fc8886de0bde83e36a84698705a625c9a
dragonfly161518 <por.nattapat.01@gmail.com>
2020-02-24 21:21:30 +0700

    post api

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,5 +10,14 @@ def hello_world():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
+@app.route('/theblindway', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    
+    return jsonify({'task': 'yepp'}), 201
+
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    
