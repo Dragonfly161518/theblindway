99cf9905d21165706a2bbb81b95991ee717955da
dragonfly161518 <por.nattapat.01@gmail.com>
2020-02-24 23:41:38 +0700

    ....

--- app.py
+++ app.py
@@ -2,8 +2,20 @@ import os
 from flask import Flask
 from flask import request
 import datetime
-import pyrebase
 import numpy as np
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('/serviceAccountKey.json')
+
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred)
+
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference('fuck')
+print(ref.get())
 
 config = {
     "apiKey": "AIzaSyCoLhhc45GCqQZWFGtUCf5G_1QAmUTI_QI",
@@ -16,8 +28,8 @@ config = {
     "measurementId": "G-CMESXDR2Q5"
 }
 
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
+# firebase = pyrebase.initialize_app(config)
+# db = firebase.database()
 
 
 app = Flask(__name__)
